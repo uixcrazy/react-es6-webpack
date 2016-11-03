d1a241d8ec31f1c42ed52471485bc6404b93515b
uixcrazy <uixcrazy@gmail.com>
2016-11-03 13:39:55 +0700

    refactor (simple-tabs): change structure scss

--- app/simple-tabs/app.js
+++ app/simple-tabs/app.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import { render } from 'react-dom';
 import SimpleTabs from './components/simple-tabs';
-import './simple-tabs.scss';
+import './stylesheets/simple-tabs.scss';
 
 class App extends Component {
   constructor(props) {
